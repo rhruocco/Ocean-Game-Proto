c9a2f369b862ad49dff01558d17e4bc95c7748e5
Ryan <ruoccoryan@gmail.com>
2022-01-30 00:06:34 -0600

    work

--- Ocean Game Proto/Assets/Scripts/Movement.cs
+++ Ocean Game Proto/Assets/Scripts/Movement.cs
@@ -7,6 +7,10 @@ public class Movement : MonoBehaviour
     private Rigidbody2D myRB;
     bool grounded, mounted;
 
+    float horizontalDir;
+
+    string whatAmIRiding;
+
     [SerializeField]
     Mount mountScript;
     // Start is called before the first frame update
@@ -15,18 +19,31 @@ public class Movement : MonoBehaviour
         myRB = GetComponent<Rigidbody2D>();
         grounded = true;
         mounted = false;
+
+        horizontalDir = Input.GetAxisRaw("Horizontal");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontalDir = Input.GetAxisRaw("Horizontal");
+
         if (!mounted)
         {
             StandardMovement();
         }
         else
         {
-            MantaMovement();
+            if (whatAmIRiding.Equals("manta"))
+            {
+                MantaMovement();
+            }
+            if (whatAmIRiding.Equals("crab"))
+            {
+                CrabMovement();
+            }
+
         }
 
         //Debug.Log(mounted);
@@ -39,9 +56,21 @@ public class Movement : MonoBehaviour
         {
             grounded = true;
         }
-        if (collision.gameObject.CompareTag("mount"))
+        if (collision.gameObject.CompareTag("manta"))
         {
             mounted = true;
+            whatAmIRiding = "manta";
+
+            mountScript = collision.gameObject.GetComponent<Mount>();
+            mountScript.followPlayer(true);
+        }
+
+        if (collision.gameObject.CompareTag("crab"))
+        {
+            mounted = true;
+            whatAmIRiding = "crab";
+
+            mountScript = collision.gameObject.GetComponent<Mount>();
             mountScript.followPlayer(true);
         }
 
@@ -57,8 +86,6 @@ public class Movement : MonoBehaviour
 
     void StandardMovement()
     {
-        float horizontalDir = Input.GetAxisRaw("Horizontal");
-
         myRB.velocity = new Vector2(horizontalDir * 7f, myRB.velocity.y);
         if (Input.GetButtonDown("Jump") && grounded)
         {
@@ -68,11 +95,28 @@ public class Movement : MonoBehaviour
 
     void MantaMovement()
     {
-        myRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * 10, Input.GetAxisRaw("Vertical") * 10);
+        myRB.velocity = new Vector2(horizontalDir * 10, Input.GetAxisRaw("Vertical") * 10);
         if (Input.GetButton("Fire3"))
         {
             mounted = false;
             mountScript.followPlayer(false);
+        }
+    }
+
+    void CrabMovement()
+    {
+
+        myRB.velocity = new Vector2(horizontalDir * 5f, myRB.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (grounded)
+            {
+                myRB.velocity = new Vector2(myRB.velocity.x, 2f);
+            }
+            else
+            {
+
+            }
         }
     }
 }
