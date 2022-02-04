4720f2c6e57ea5ef327665dcd8bbcd6a353b7c68
Ryan <44453329+rhruocco@users.noreply.github.com>
2022-02-03 19:35:10 -0600

    implemented karen sprites

---
+++ Ocean Game Proto/Assets/Scripts/Pearl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pearl : MonoBehaviour
+{
+    [SerializeField]
+    string Nextscene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            SceneManager.LoadScene(Nextscene);
+        }
+    }
+}
